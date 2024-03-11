9a6ab6a775c5f588dffb9fc503eb82f1c54872e2
G0v0run <162915104+G0v0run@users.noreply.github.com>
2024-03-11 04:46:45 +0200

    Delete Koots directory

--- Koots/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class KootsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Koots'
-
-    def ready(self):
-        import Koots.Chehovs_bot
--- Koots/management/commands/run_bot.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from Koots.Chehovs_bot import start_bot  # Импортируем функцию start_bot из вашего файла Chehovs_bot.py
-
-class Command(BaseCommand):
-    help = 'Запускает бота Telegram'
-
-    def handle(self, *args, **options):
-        start_bot()  # Запускаем вашего бота
